56b19551a85a196a2b8ece6ff7cdd5f0d02023f4
Roberto Melo <robertoreboucasdemelo@gmail.com>
2023-03-13 17:03:59 -0300

    Add Inline Processor

--- simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
+++ simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
@@ -1,15 +1,25 @@
 package com.moduretick.simplefilepoller.components;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LegacyFileRoute extends RouteBuilder {
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
 	public void configure() throws Exception {
 		from("file:src/data/input?fileName=inputFile.txt")
 		.routeId("legacyFileMoveRouteId")
+		.process(exchange ->{
+			String fileData = exchange.getIn().getBody(String.class);
+			logger.info("This is the read fileData: " + fileData);
+		})
 		.to("file:src/data/output?fileName=outputFile.txt");
 		
 	}
