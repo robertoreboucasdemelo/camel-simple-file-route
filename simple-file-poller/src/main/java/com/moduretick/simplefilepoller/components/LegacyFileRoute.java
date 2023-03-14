9eb4af46d0921f3a3f2040ae8b21286c4bdf0e85
Roberto Melo <robertoreboucasdemelo@gmail.com>
2023-03-14 13:51:52 -0300

    Read txt File

--- simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
+++ simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
@@ -1,7 +1,5 @@
 package com.moduretick.simplefilepoller.components;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
