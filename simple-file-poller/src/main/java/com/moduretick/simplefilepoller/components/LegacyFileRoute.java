be5a9716314d63b96f62619edb6d25285786b075
Roberto Melo <robertoreboucasdemelo@gmail.com>
2023-03-15 09:08:15 -0300

    Using the Spitter EIP

--- simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
+++ simple-file-poller/src/main/java/com/moduretick/simplefilepoller/components/LegacyFileRoute.java
@@ -12,13 +12,14 @@ public class LegacyFileRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("file:src/data/input?fileName=inputFile.txt")
+		from("file:src/data/input?fileName=inputFile.csv")
 		.routeId("legacyFileMoveRouteId")
+		.split(body().tokenize("\n", 1, true))
 		.process(exchange ->{
 			String fileData = exchange.getIn().getBody(String.class);
 			logger.info("This is the read fileData: " + fileData);
 		})
-		.to("file:src/data/output?fileName=outputFile.txt");
+		.to("file:src/data/output?fileName=outputFile.csv&fileExist=append&appendChars=\\n");
 		
 	}
 
